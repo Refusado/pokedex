a1d915928c87b552ee9986810b02d9b70b883977
Renan <orefusado@gmail.com>
2022-12-07 04:45:08 -0300

    terminando o desenvolvimento do projeto

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,47 @@
-
+function convertPokemonTypeToLi(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
 
 function convertPokemonToHTML(pokemon) {
     return `
-        <li class="pokemon">
-            <span class="number">#001</span>
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
             <div class="detail">
                 <ol class="types">
-                    <li class="type">grass</li>
-                    <li class="type">poison</li>
+                    ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
                 </ol>
 
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                <img src="${pokemon.img}"
                     alt="${pokemon.name}">
             </div>
         </li>`;
 }
 
 const pokemonsContainer = document.getElementById('pokemonList');
+const loadBtn           = document.getElementById('loadMore-btn');
+let offset      = 0;
+let limit       = 5;
+let maxItems    = 151;
 
-pokeApi.getPokemons().then((pokemonList) => {
-    const listItems = [];
+function loadPokemonItems(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemonList = []) => {
+        const newHTML = pokemonList.map(convertPokemonToHTML).join('');
+        pokemonsContainer.innerHTML += newHTML;
+    });
+}
 
-    for (let i = 0; i < pokemonList.length; i++) {
-        const pokemon = pokemonList[i];
-        listItems.push(convertPokemonToHTML(pokemon));
+loadPokemonItems(offset, 5);
+
+loadBtn.addEventListener('click', () => {
+    offset += limit;
+    const qtdNextPage = offset + limit;
+
+    if (qtdNextPage >= maxItems) {
+        loadPokemonItems(offset, maxItems - offset);
+        loadBtn.parentElement.removeChild(loadBtn);
+    } else {
+        loadPokemonItems(offset, 5);
     }
-    console.log(listItems);
-})
-
-console.log(10 + 90);
+});
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,9 +1,33 @@
 const pokeApi = {};
 
+pokeApi.convertPokeApiToPokemon = (pokeDetail) => {
+    const pokemon = new Pokemon();
+
+    pokemon.number  = pokeDetail.id;
+    pokemon.name    = pokeDetail.name;
+
+    const types     = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
+    const [ type ]  = types;
+
+    pokemon.types   = types;
+    pokemon.type    = type;
+    pokemon.img     = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
+pokeApi.getPokemonDetails = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(pokeApi.convertPokeApiToPokemon);
+}
+
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
     const url   = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
-        .catch((error) => console.log(error));
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetails))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonDetails) => pokemonDetails);
 }
